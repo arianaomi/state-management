9ee463edb5d49917e468cdc5762c6f287b620e39
arianaomi <nlopez@gmail.comm>
2023-02-22 13:55:41 -0600

    create store

--- src/stores/pokemon.js
+++ src/stores/pokemon.js
@@ -1,7 +1,17 @@
 import { create } from "zustand";
 
 const usePokemonsStore = create((set,get)=>({
-  
+  pokemons:[],
+  pokemonDetail:[],
+  isLoading: false,
+  errorMessage: '',
+  hasError:false,
+
+  getPokemons: async () => {
+    try{
+      set({isLoading: false, hasError:'', hasError: false})
+  } catch{}},
+  getPokemonsDetail: async () => {},
 }))
 
 export default usePokemonsStore;
